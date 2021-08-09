dea594d4cbc570f520c4b53be471da3d56cb1398
harshavardhan-03 <83867245+harshavardhan-03@users.noreply.github.com>
2021-08-09 21:53:55 +0530

    Scam Tester

--- src/com/xworkz/tester/ScamOrderedByNameTester.java
+++ src/com/xworkz/tester/ScamOrderedByNameTester.java
@@ -16,24 +16,24 @@ public class ScamOrderedByNameTester {
 	public static void main(String[] args) {
 
 		try (Connection connection = DriverManager.getConnection(url, username, password)) {
+			connection.setAutoCommit(false);
 			String query = "SELECT * FROM scam ORDER BY s_name";
 			Statement statement = connection.createStatement();
 			statement.execute(query);
+			connection.commit();
 			ResultSet resultSet = statement.getResultSet();
 			ScamDTO dto = null;
 			while (resultSet.next()) {
-				connection.setAutoCommit(false);
-				int anu = resultSet.getInt("s_id");
-				String sha = resultSet.getString("s_name");
+				int id = resultSet.getInt("s_id");
+				String name = resultSet.getString("s_name");
 				String type = resultSet.getString("s_type");
 				Date year = resultSet.getDate("s_year");
 				String by = resultSet.getString("s_by");
 				double amount = resultSet.getDouble("s_amount");
 				String location = resultSet.getString("s_location");
 				String description = resultSet.getString("s_description");
-				dto = new ScamDTO(anu, sha, type, year, by, amount, location, description);
+				dto = new ScamDTO(id, name, type, year, by, amount, location, description);
 				System.out.println(dto);
-				connection.commit();
 			}
 		} catch (SQLException e) {
 			e.printStackTrace();
